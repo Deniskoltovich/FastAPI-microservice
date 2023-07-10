e04d1ace5968d79f24bd2a1aad1860f2cbbb7128
Denis Koltovich <koltovich1123@gmail.com>
2023-07-10 10:20:20 +0300

    edited entrypoint in app/main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,3 @@ import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
-
-if __name__ == "__main__":
-    uvicorn.run(app, host=os.environ.get('APP_HOST'), port=int(os.environ.get('APP_PORT')))
