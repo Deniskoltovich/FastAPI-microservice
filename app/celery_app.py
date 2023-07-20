6e1f00a670a1ea54b7d515c8fc4d121510437910
Denis Koltovich <koltovich1123@gmail.com>
2023-07-20 13:29:48 +0300

    minor changes

--- app/celery_app.py
+++ app/celery_app.py
@@ -9,24 +9,24 @@ redis_url = settings.BROKER_REDIS_URL
 
 celery_app = Celery(__name__, broker=redis_url, backend=redis_url)
 celery_app.autodiscover_tasks(['app.assets.tasks'])
-#
-# celery_app.conf.beat_schedule = {
-#     'parse-every-minute': {
-#         'task': 'app.assets.tasks.parse_assets',
-#         'schedule': crontab(),
-#         'args': (
-#             (
-#                 'AAA',
-#                 'IBM',
-#                 'AAPL',
-#                 'A',
-#                 'ADBE',
-#                 'AVK',
-#                 'BL',
-#             ),
-#         ),
-#     }
-# }
+
+celery_app.conf.beat_schedule = {
+    'parse-every-minute': {
+        'task': 'app.assets.tasks.parse_assets',
+        'schedule': crontab(),
+        'args': (
+            (
+                'AAA',
+                'IBM',
+                'AAPL',
+                'A',
+                'ADBE',
+                'AVK',
+                'BL',
+            ),
+        ),
+    }
+}
 
 celery_app.conf.timezone = 'UTC'
 
